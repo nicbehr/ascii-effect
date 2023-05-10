c30f861f43e7256b8c79a5d5d04aab0279ff2217
NAB <n-beh@gmx.de>
2023-05-10 15:46:01 +0200

    added image scaling and controls

--- AsciiEffect.js
+++ AsciiEffect.js
@@ -1,4 +1,4 @@
-import { Cell } from "./Cell";
+import { Cell } from "./Cell.js";
 
 export class AsciiEffect {
     #imageCellArray = [];
@@ -14,22 +14,34 @@ export class AsciiEffect {
         this.#width = width;
         this.#height = height;
         this.#image = image;
-        this.#ctx.drawImage(image, 0, 0, this.#width, this.#height);
+        this.#drawImageScaled(image, this.#ctx);
         this.#pixels = this.#ctx.getImageData(0, 0, this.#width, this.#height);
+    };
+
+    #drawImageScaled(img, ctx) {
+        var canvas = ctx.canvas;
+        var hRatio = window.innerWidth / img.width;
+        var vRatio = window.innerHeight / img.height;
+        var ratio = Math.min(hRatio, vRatio);
+        var centerShift_x = (window.innerWidth - img.width * ratio) / 2;
+        var centerShift_y = (window.innerHeight - img.height * ratio) / 2;
+        this.#ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.#ctx.drawImage(img, 0, 0, img.width, img.height,
+            centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);
     }
 
-    #converToSymbol(color) {
+    #convertToSymbol(color) {
         const symbols = ["@", "*", "+", "#", "&", "%", "_", ":", "$", "/", "-", "X", "W"];
         const thresholds = [250];
         for (let i = symbols.length - 1; i > 0; i--) {
             thresholds.push((250 / symbols.length) * i)
-        }
-        thresholds.forEach(threshold => {
-            if (color < threshold) {
-                return symbols[thresholds.indexOf(threshold)];
+        };
+        for (let i = 0; i < thresholds.length; i++) {
+            if (color >= thresholds[i]) {
+                return symbols[i];
             }
-        })
-    }
+        }
+    };
     #scanImage = function (cellSize) {
         this.#imageCellArray = [];
         for (let y = 0; y < this.#pixels.height; y += cellSize) {
@@ -45,7 +57,7 @@ export class AsciiEffect {
                     const total = (red + green + blue);
                     const averageColorvalue = total / 3;
                     const color = "rgb(" + red + "," + green + "," + blue + ")";
-                    const symbol = this.#converToSymbol(averageColorvalue);
+                    const symbol = this.#convertToSymbol(averageColorvalue);
                     if (total > 200) {
                         this.#imageCellArray.push(new Cell(x, y, symbol, color));
                     }
@@ -53,7 +65,19 @@ export class AsciiEffect {
             }
         }
     }
+    #drawAscii() {
+        this.#ctx.clearRect(0, 0, this.#width, this.#height);
+        this.#imageCellArray.forEach(imageCell => {
+            imageCell.draw(this.#ctx)
+        });
+
+    }
     draw(cellSize) {
+        if (cellSize == 1) {
+            this.#drawImageScaled(this.#image, this.#ctx);
+            return;
+        }
         this.#scanImage(cellSize)
+        this.#drawAscii()
     }
 }
